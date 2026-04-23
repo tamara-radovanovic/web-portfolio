be3f3ca5d60ef1957a4a8422b19902ebaa4b36ec
Tamara Radovanovic <tamararadovanovic02@gmail.com>
2026-04-23 16:51:17 +0200

    add NotFoundPage

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,13 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import styles from './NotFoundPage.module.css'
 
-function NotFoundPage(){
-    return(
-        <div className="p-8">
-            <h1 className="text-2xl">404 - Page Not Found</h1>
-            <Link to="/" className="text-blue-500 underline mt-4 block">
-                Go to Home Page
-            </Link>
+export default function NotFoundPage() {
+  return (
+    <div className={styles.page}>
+      <div className={styles.container}>
+
+        <p className={styles.code}>404</p>
+        <h1 className={styles.title}>Page not found</h1>
+        <p className={styles.text}>
+          Looks like this page doesn't exist. Let's get you back on track.
+        </p>
+
+        <div className={styles.actions}>
+          <Link to="/" className={styles.btnPrimary}>
+            ← Back to Home
+          </Link>
         </div>
-    )
+
+      </div>
+    </div>
+  )
 }
- export default NotFoundPage;
